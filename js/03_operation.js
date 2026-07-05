b21fdf956dde14a5b5e0911945bb0b7ed6e06225
Pratham Bairagi <prathamg1123@gmail.com>
2026-07-05 14:26:45 +0530

    initial commit

---
+++ js/03_operation.js
@@ -0,0 +1,11 @@
+//operationn and conversionn
+
+let age = 39
+let stringAge = String(age)
+
+console.log(typeof stringAge)
+console.log(stringAge)
+
+
+//*****************************Operations******************************* */
+
